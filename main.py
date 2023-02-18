97987fbbc7baee1e07e02ccf4e69daf4382a3e4a
Cesar Agudelo <cesaragudelo811@gmail.com>
2023-02-18 11:52:46 -0500

    new update

---
+++ main.py
@@ -0,0 +1,8 @@
+numeroUno = int (input('Digite el número:'))
+numeroDos = int (input('Digita segundo número:'))
+
+print(f'El segundo número es: {numeroDos}')
+
+
+suma = numeroUno + numeroDos
+print(f'la suma es {suma}')
